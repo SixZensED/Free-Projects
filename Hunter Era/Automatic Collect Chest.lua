2c3c227a2f2b414a4fdff7a99a167fd0499a2f67
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-07-18 03:18:46 +0700

    Update Automatic Collect Chest.lua

--- Hunter Era/Automatic Collect Chest.lua
+++ Hunter Era/Automatic Collect Chest.lua
@@ -4,12 +4,14 @@ local interact = function()
 	game:service('VirtualInputManager'):SendKeyEvent(false, "E", false, game)
 end
 
-for i,v in pairs(Workspace.Ambients.Chests:GetChildren()) do
-	if v:IsA("Model") and v:FindFirstChild("chest").Transparency == 0 then
-		repeat task.wait()
-			game:GetService("Players").LocalPlayer.Character:PivotTo(v:FindFirstChild("chest").CFrame)
-			interact()
-			task.wait(0.5)
-		until v:FindFirstChild("chest").Transparency > 0 
+for i,v in pairs(Workspace:GetDescendants()) do
+	if v.Name == "chest" and v:IsA("Model") then
+		if v:FindFirstChild("chest").Transparency == 0 then
+			repeat task.wait()
+				game:GetService("Players").LocalPlayer.Character:PivotTo(v:FindFirstChild("chest").CFrame)
+				interact()
+				task.wait(0.15)
+			until v:FindFirstChild("chest").Transparency > 0 
+		end
 	end
 end
