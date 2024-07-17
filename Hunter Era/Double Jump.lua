4844b823daad9e44c92930754668ea3507e458f7
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-07-18 02:40:21 +0700

    Delete Hunter Era/Double Jump.lua

--- Hunter Era/Double Jump.lua
+++
@@ -1,6 +0,0 @@
-local state = game:GetService("Players").LocalPlayer:FindFirstChild("Skills")
-local doublejump = state and state:GetAttribute("DoubleJump") == false
-
-if doublejump then
-	state:SetAttribute("DoubleJump", true)
-end
