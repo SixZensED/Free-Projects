8598ff21fef4c64384ccf9b7cbc6db700fe27eb9
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-07-21 01:53:29 +0700

    Create One shot melee.lua

---
+++ 4 King Gangster/One shot melee.lua
@@ -0,0 +1,6 @@
+local test = require(game:GetService("Players").LocalPlayer.Character["\224\184\171\224\184\161\224\184\177\224\184\148"].MainModule["1"])
+
+test.BaseDamage = {500,1000}
+test.AttackDelay = 0
+
+-- equip melee first
